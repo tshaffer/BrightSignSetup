68378ae453199ec0a6c8dbab2adec1b0f976d31f
Classroom Tech User <classroomtech@is-ct-a014503.local>
2015-02-13 19:41:21 -0800

    Initial Commit

---
+++ iOS/iOS_JTR/iOS_JTR/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iOS_JTR
+//
+//  Created by Classroom Tech User on 2/13/15.
+//  Copyright (c) 2015 Classroom Tech User. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
