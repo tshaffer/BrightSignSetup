6157a8e48a2723d7d144051ee42da178c938b259
Ted Shaffer <ted@brightsign.biz>
2015-10-05 15:29:42 -0700

    initial testing of handlers / jquery

--- js/brightSignSetup.js
+++ js/brightSignSetup.js
@@ -1,6 +1,15 @@
 /**
  * Created by Ted Shaffer on 10/5/2015.
  */
+function createSetupFiles() {
+    console.log("create setup files");
+
+    var unitName = $("#txtBoxUnitName").val();
+    var unitDescription = $("#txtBoxUnitDescription").val();
+
+    var customization = $("input[name=customization]:checked").val();
+}
+
 $(document).ready(function () {
     console.log("hello world");
 });
