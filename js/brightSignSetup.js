a6022fbb9327439c14e99c0955d18b84263ff0b3
Ted Shaffer <ted@brightsign.biz>
2015-10-06 09:37:42 -0700

    Ongoing development

--- js/brightSignSetup.js
+++ js/brightSignSetup.js
@@ -4,12 +4,61 @@
 function createSetupFiles() {
     console.log("create setup files");
 
+    // retrieve settings
     var unitName = $("#txtBoxUnitName").val();
     var unitDescription = $("#txtBoxUnitDescription").val();
-
     var customization = $("input[name=customization]:checked").val();
+
+    var enableWireless = $("#checkBoxEnableWireless").is(':checked');
+    var ssid = $("#txtBoxSSID").val();
+    var securityKey = $("#txtBoxSecurityKey").val();
+
+    var networkConnectionPriority = $("input[name=networkConnectionPriority]:checked").val();
+
+    var timeZone = $("#selectTimeZone").val();
+    var timeServer = $("#txtBoxTimeServer").val();
+
+    var enableDWS = $("#checkBoxEnableDWS").is(':checked');
+    var dwsPassword = $("#txtBoxDWSPassword").val();
+
+    var enableLWS = $("#checkBoxEnableLWS").is(':checked');
+    var lwsUserName = $("#txtBoxLWSUserName").val();
+    var lwsPassword = $("#txtBoxLWSPassword").val();
+
+    var bsnGroup = $("#selectBSNGroup").val();
+    var contentCheckInterval = $("#selectContentCheckFrequency").val();
+    var updateHealthInterval = $("#selectUpdateHealthFrequency").val();
+
+    var enablePlaybackLogging = $("#checkBoxEnablePlaybackLogging").is(':checked');
+    var enableEventLogging = $("#checkBoxEnableEventLogging").is(':checked');
+    var enableStateLogging = $("#checkBoxEnableStateLogging").is(':checked');
+    var enableDiagnosticLogging = $("#checkBoxEnableDiagnosticLogging").is(':checked');
+
+    var uploadLogsOnStartup = $("#checkBoxUploadLogsOnStartup").is(':checked');
+    var uploadLogsAtSpecificTimeEachDay = $("#checkBoxUploadLogsAtSpecificTimeEachDay").is(':checked');
 }
 
 $(document).ready(function () {
     console.log("hello world");
+
+    // add handlers
+
+    $("#checkBoxEnableWireless").change(function() {
+        var enableWireless = $("#checkBoxEnableWireless").is(':checked');
+        $("#txtBoxSSID").prop("disabled", !enableWireless);
+        $("#txtBoxSecurityKey").prop("disabled", !enableWireless);
+    });
+
+    $("#checkBoxEnableDWS").change(function() {
+        var enableDWS = $("#checkBoxEnableDWS").is(':checked');
+        $("#txtBoxDWSPassword").prop("disabled", !enableDWS);
+    });
+
+    $("#checkBoxEnableLWS").change(function() {
+        var enableLWS = $("#checkBoxEnableLWS").is(':checked');
+        $("#txtBoxLWSUserName").prop("disabled", !enableLWS);
+        $("#txtBoxLWSPassword").prop("disabled", !enableLWS);
+    });
+
+
 });
