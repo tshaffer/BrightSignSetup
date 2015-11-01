819d7c0b819a2b1f95b7c0bf3cf41ceba2122834
Ted Shaffer <ted@brightsign.biz>
2015-11-01 06:23:15 -0800

    added a line of diagnostic output

--- jtr/src/remoteWebSite/js/recordedShowsController.js
+++ jtr/src/remoteWebSite/js/recordedShowsController.js
@@ -32,6 +32,7 @@ define(['recordedShowsModel','recordedShowsView'], function (RecordedShowsModel,
             this.recordedShowsModel.fetch({
                 success: function () {
                     console.log("JSON file load was successful", self.recordedShowsModel);
+                    console.log("number of recorded shows = " + self.recordedShowsModel.models.length.toString());
                 },
                 error: function () {
                     console.log('There was some error in loading and processing the JSON file');
