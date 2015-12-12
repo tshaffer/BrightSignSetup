88ff89f0680087d0da85ab165b915587aa84c13c
Ted Shaffer <ted@brightsign.biz>
2015-12-12 06:39:55 -0800

    populate footer using ng-repeat

--- jtr/src/angularRemoteWebSite/app.js
+++ jtr/src/angularRemoteWebSite/app.js
@@ -41,9 +41,27 @@ myApp.config(['$routeProvider', function ($routeProvider) {
 }]);
 
 myApp.controller('FooterCtrl', ['$scope', '$log', function($scope, $log) {
-     $scope.invokeTrickMode = function(trickMode) {
-            console.log("trickMode invoked: " + trickMode);
-     };
+
+    $scope.invokeHome = function() {
+        console.log("home invoked");
+    }
+
+    $scope.invokeTrickMode = function(trickMode) {
+        console.log("trickMode invoked: " + trickMode);
+    };
+
+    $scope.trickModes = [
+
+        { command: "record", font: "glyphicon-record" },
+        { command: "stop", font: "glyphicon-stop" },
+        { command: "rewind", font: "glyphicon-backward" },
+        { command: "instantReplay", font: "glyphicon-step-backward" },
+        { command: "pause", font: "glyphicon-pause" },
+        { command: "play", font: "glyphicon-play" },
+        { command: "quickSkip", font: "glyphicon-step-forward" },
+        { command: "fastForward", font: "glyphicon-forward" }
+    ];
+
 }]);
 
 myApp.controller('recordingsController', ['$scope', '$log', function($scope, $log) {
