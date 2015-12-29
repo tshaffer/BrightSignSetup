492fa3291ef66b94574821293b2e7b0ae9325c0d
Ted Shaffer <ted@brightsign.biz>
2015-12-29 12:56:25 -0800

    Playing around a little to understand it better.

--- jtr/src/meteorWebSite/client/stations/stations.component.js
+++ jtr/src/meteorWebSite/client/stations/stations.component.js
@@ -5,10 +5,37 @@ angular.module('jtr').directive('stations', function () {
     return {
         restrict: 'E',
         templateUrl: 'client/stations/stations.html',
-        controllerAs: 'stations',
+        controllerAs: 'stationList',
         controller: function ($scope, $reactive) {
             $reactive(this).attach($scope);
 
+            this.newStation = {};
+
+            this.getStationCount = function() {
+                //return this.allStations.length;
+                return 2;
+            }
+
+            this.helpers({
+                stations: () => {
+
+                    return Stations.find({});
+
+                    //var theStations = Stations.find({});
+                    //
+                    //this.allStations = [];
+                    //
+                    //var self = this;
+                    //
+                    //angular.forEach(theStations, function(station, stationIndex) {
+                    //    self.allStations.push(station);
+                    //});
+                    //
+                    //this.numStations = this.allStations.length;
+                    //
+                    //return this.allStations;
+                }
+            })
             //this.newParty = {};
             //
             //this.helpers({
