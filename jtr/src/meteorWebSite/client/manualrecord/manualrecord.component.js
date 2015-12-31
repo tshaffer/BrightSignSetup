601b330734a0d8817e8724ecd5ca99fb4c6bc61c
Ted Shaffer <ted@brightsign.biz>
2015-12-30 18:03:28 -0800

    ongoing work on manual record

--- jtr/src/meteorWebSite/client/manualrecord/manualrecord.component.js
+++ jtr/src/meteorWebSite/client/manualrecord/manualrecord.component.js
@@ -9,39 +9,49 @@ angular.module('jtr').directive('manualrecord', ['$jtrStationsService', function
         controller: function ($scope, $reactive) {
             $reactive(this).attach($scope);
 
-            //this.numStations = 2;
+            this.invokeManualRecord = function() {
+                console.log("invoke manual record");
+                console.log("title is " + this.title);
 
-            //this.getStationCount = function() {
-            //    //return this.allStations.length;
-            //    return 3;
-            //}
+                var date = this.date;
+                var time = this.time;
 
-            //this.stations = function() {
-            //    return $jtrStationsService.getStations();
-            //}
-            this.helpers({
-                stations: () => {
+                var dateTime = date.clearTime();
+                dateTime.set( {hour: time.getHours(), minute: time.getMinutes(), second: 0, millisecond: 0 } );
 
-                    //$jtrStationsService.sayHello();
-                    //
-                    //return $jtrStationsService.getStations();
-                    //return Stations.find({});
+                //var dateTimeStr = dateTime.toString();
+                //var dateTimeStr = date + " " + time;
 
-                    //var theStations = Stations.find({});
-                    //
-                    //this.allStations = [];
-                    //
-                    //var self = this;
-                    //
-                    //angular.forEach(theStations, function(station, stationIndex) {
-                    //    self.allStations.push(station);
-                    //});
-                    //
-                    //this.numStations = this.allStations.length;
-                    //
-                    //return this.allStations;
-                },
-            })
+                // required for iOS devices - http://stackoverflow.com/questions/13363673/javascript-date-is-invalid-on-ios
+                //var compatibleDateTimeStr = dateTimeStr.replace(/-/g, '/');
+                //var dateObj = new Date(compatibleDateTimeStr);
+
+                var duration = this.duration;
+                var inputSource = this.inputSource;
+                var channel = this.channel;
+//]               var title = this.getRecordingTitle("#manualRecordTitle", dateObj, inputSource, channel);
+                var title = this.title;
+
+                // check to see if recording is in the past
+                //var dtEndOfRecording = addMinutes(dateObj, duration);
+                //var dtEndOfRecording = new Date(dateObj).addMinutes(Number(duration));
+                var dtEndOfRecording = dateTime.addMinutes(Number(duration));
+                var now = new Date();
+
+                var millisecondsUntilEndOfRecording = dtEndOfRecording - now;
+                if (millisecondsUntilEndOfRecording < 0) {
+                    alert("Recording time is in the past - change the date/time and try again.");
+                    return false;
+                }
+
+                this.model.set('title', title);
+                this.model.set('dateTime', dateObj);
+                this.model.set('duration', duration);
+                this.model.set('inputSource', inputSource);
+                this.model.set('channel', channel);
+                this.model.set('scheduledSeriesRecordingId', -1);
+
+            };
         }
     }
 }]);
