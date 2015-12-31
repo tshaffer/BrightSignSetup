28c1cd724590021fd1c6b76105c1f4c29e6a059e
Ted Shaffer <ted@brightsign.biz>
2015-12-31 05:44:33 -0800

    write manual record information to scheduledRecordings db collection

--- jtr/src/meteorWebSite/client/manualrecord/manualrecord.component.js
+++ jtr/src/meteorWebSite/client/manualrecord/manualrecord.component.js
@@ -9,6 +9,23 @@ angular.module('jtr').directive('manualrecord', ['$jtrStationsService', function
         controller: function ($scope, $reactive) {
             $reactive(this).attach($scope);
 
+            this.getRecordingTitle = function (title, dateObj, inputSource, channel) {
+
+                if (!title) {
+                    title = 'MR ' + dateObj.getFullYear() + "-" + twoDigitFormat((dateObj.getMonth() + 1)) + "-" + twoDigitFormat(dateObj.getDate()) + " " + twoDigitFormat(dateObj.getHours()) + ":" + twoDigitFormat(dateObj.getMinutes());
+                    if (this.inputSource == "tuner") {
+                        title += " Channel " + channel;
+                    } else if (this.inputSource == "tivo") {
+                        title += " Tivo";
+                    } else {
+                        title += " Roku";
+                    }
+                }
+
+                return title;
+            };
+
+
             this.invokeManualRecord = function() {
                 console.log("invoke manual record");
                 console.log("title is " + this.title);
@@ -16,40 +33,33 @@ angular.module('jtr').directive('manualrecord', ['$jtrStationsService', function
                 var date = this.date;
                 var time = this.time;
 
-                var dateTime = date.clearTime();
-                dateTime.set( {hour: time.getHours(), minute: time.getMinutes(), second: 0, millisecond: 0 } );
-
-                //var dateTimeStr = dateTime.toString();
-                //var dateTimeStr = date + " " + time;
-
-                // required for iOS devices - http://stackoverflow.com/questions/13363673/javascript-date-is-invalid-on-ios
-                //var compatibleDateTimeStr = dateTimeStr.replace(/-/g, '/');
-                //var dateObj = new Date(compatibleDateTimeStr);
-
-                var duration = this.duration;
-                var inputSource = this.inputSource;
-                var channel = this.channel;
-//]               var title = this.getRecordingTitle("#manualRecordTitle", dateObj, inputSource, channel);
-                var title = this.title;
+                date.clearTime();
+                var dateObj = date.set({
+                    millisecond: 0,
+                    second: 0,
+                    minute: time.getMinutes(),
+                    hour: time.getHours()
+                });
 
                 // check to see if recording is in the past
-                //var dtEndOfRecording = addMinutes(dateObj, duration);
-                //var dtEndOfRecording = new Date(dateObj).addMinutes(Number(duration));
-                var dtEndOfRecording = dateTime.addMinutes(Number(duration));
+                var dtEndOfRecording = new Date(dateObj).addMinutes(Number(this.duration));
                 var now = new Date();
 
                 var millisecondsUntilEndOfRecording = dtEndOfRecording - now;
                 if (millisecondsUntilEndOfRecording < 0) {
                     alert("Recording time is in the past - change the date/time and try again.");
-                    return false;
                 }
 
-                this.model.set('title', title);
-                this.model.set('dateTime', dateObj);
-                this.model.set('duration', duration);
-                this.model.set('inputSource', inputSource);
-                this.model.set('channel', channel);
-                this.model.set('scheduledSeriesRecordingId', -1);
+                this.manualRecordingParameters = {}
+                this.manualRecordingParameters.title = this.getRecordingTitle(this.title, dateObj, this.inputSource, this.channel);
+                this.manualRecordingParameters.dateTime = dateObj;
+                this.manualRecordingParameters.duration = this.duration;
+                this.manualRecordingParameters.inputSource = this.inputSource;
+                this.manualRecordingParameters.channel = this.channel;
+                this.manualRecordingParameters.scheduledSeriesRecordingId = -1;
+
+                //$jtrServerService.manualRecording($scope.manualRecordingParameters);
+                ScheduledRecordings.insert(this.manualRecordingParameters);
 
             };
         }
