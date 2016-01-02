f43da243fdfefb18d1224fa256b234611f9fadf6
Ted Shaffer <ted@brightsign.biz>
2016-01-02 05:10:53 -0800

    add epg.js; remove code from app.js

---
+++ jtr/src/meteorWebSite/server/lib/epg.js
@@ -0,0 +1,31 @@
+/**
+ * Created by tedshaffer on 1/2/16.
+ */
+
+function getSchedulesDirectToken(nextFunction) {
+
+    var url = "https://json.schedulesdirect.org/20141201/token";
+
+    HTTP.call("POST", url,
+        {
+            data: {"username": "jtrDev", "password": "3bacdc30b9598fb498dfefc00b2f2ad52150eef4"},
+            headers: {"User-Agent": "jtr"}
+        },
+        function (error, result) {
+            if (error) {
+                console.log("error");
+                console.log(error);
+            }
+            else {
+                //console.log("Schedules direct token");
+                //console.log(JSON.stringify(result, null, 4));
+
+                schedulesDirectToken = result.data.token;
+                console.log("Schedules direct token = " + schedulesDirectToken);
+
+                if (nextFunction != null) {
+                    nextFunction();
+                }
+            }
+        });
+}
