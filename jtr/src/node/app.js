a6c86121f75f5e16ba468be0871ff35cc3fce063
Ted Shaffer <ted@brightsign.biz>
2016-01-09 08:51:22 -0800

    completed code to synchronize recordings table between jtr and mongo. make more general purpose in the future.

--- jtr/src/node/app.js
+++ jtr/src/node/app.js
@@ -18,7 +18,7 @@ var recordingSchema = new Schema({
     RecordingId: Number,
     StartDateTime: Date,
     Title: String,
-    TranscodeComplete: Boolean  
+    TranscodeComplete: Boolean
 });
 
 var Recording = mongoose.model('Recording', recordingSchema);
@@ -31,67 +31,69 @@ var bigScreenJtrRecordingsOnMongo = {};
 var bigScreenJtrNativeRecordings = {};
 
 // retrieve recordings on BigScreenJtr that are in the mongodb
-Recording.find({ 'JtrStorageDevice': 'BigScreenJtr' }, function (err, recordings) {
+Recording.find({'JtrStorageDevice': 'BigScreenJtr'}, function (err, recordings) {
     if (err) throw err;
-    recordings.forEach(function(recording) {
-      bigScreenJtrRecordingsOnMongo[recording.RecordingId] = recording;
+    recordings.forEach(function (recording) {
+        bigScreenJtrRecordingsOnMongo[recording.RecordingId] = recording;
     });
 
     // next, retrieve recordings from jtr
     var url = baseUrl + "getRecordings";
     request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var recordingsResponse = JSON.parse(body);
-        var freespace = recordingsResponse.freespace;
-        var recordings = recordingsResponse.recordings;
-        recordings.forEach(function (recording) {
-          bigScreenJtrNativeRecordings[recording.RecordingId] = recording;
-        });
+        if (!error && response.statusCode == 200) {
+            var recordingsResponse = JSON.parse(body);
+            var freespace = recordingsResponse.freespace;
+            var recordings = recordingsResponse.recordings;
+            recordings.forEach(function (recording) {
+                bigScreenJtrNativeRecordings[recording.RecordingId] = recording;
+            });
 
-        // remove any recordings that exist on mongodb but not jtr db from the mongo db
-        for (var key in bigScreenJtrRecordingsOnMongo) {
-          if (bigScreenJtrRecordingsOnMongo.hasOwnProperty(key)) {
-            if (!(key in bigScreenJtrNativeRecordings)) {
-              var recordingToRemoveFromMongo = bigScreenJtrRecordingsOnMongo[key];
-                console.log("Remove " + recordingToRemoveFromMongo.Title + " from mongo");
+            // remove any recordings that exist on mongodb but not jtr db from the mongo db
+            for (var key in bigScreenJtrRecordingsOnMongo) {
+                if (bigScreenJtrRecordingsOnMongo.hasOwnProperty(key)) {
+                    if (!(key in bigScreenJtrNativeRecordings)) {
+                        var recordingToRemoveFromMongo = bigScreenJtrRecordingsOnMongo[key];
+                        console.log("Remove " + recordingToRemoveFromMongo.Title + " from mongo");
+                        Recording.remove({_id: recordingToRemoveFromMongo._id}, function(err) {
+                            if (err) throw err;
+                        });
+                    }
+                }
             }
-          }
-        }
 
-        // add any recordings that exist on jtr but not mongo db to the mongo db
-        for (var key in bigScreenJtrNativeRecordings) {
-          if (bigScreenJtrNativeRecordings.hasOwnProperty(key)) {
-            if (!(key in bigScreenJtrRecordingsOnMongo)) {
-              var recording = bigScreenJtrNativeRecordings[key];
+            // add any recordings that exist on jtr but not mongo db to the mongo db
+            for (var key in bigScreenJtrNativeRecordings) {
+                if (bigScreenJtrNativeRecordings.hasOwnProperty(key)) {
+                    if (!(key in bigScreenJtrRecordingsOnMongo)) {
+                        var recording = bigScreenJtrNativeRecordings[key];
 
-              console.log("Add " + recording.Title + " to mongo");
+                        console.log("Add " + recording.Title + " to mongo");
 
-              var recordingForDB =  Recording({
-                Duration: recording.Duration,
-                FileName: recording.FileName,
-                HLSSegmentationComplete: recording.HLSSegmentationComplete === 1 ? true : false,
-                HLSUrl: recording.HLSUrl,
-                JtrStorageDevice: "BigScreenJtr",
-                LastViewedPosition: recording.LastViewedPosition,
-                path: recording.path,
-                RecordingId: recording.RecordingId,
-                StartDateTime: recording.StartDateTime,
-                Title: recording.Title,
-                TranscodeComplete: recording.TranscodeComplete === 1 ? true : false
-              });
+                        var recordingForDB = Recording({
+                            Duration: recording.Duration,
+                            FileName: recording.FileName,
+                            HLSSegmentationComplete: recording.HLSSegmentationComplete === 1 ? true : false,
+                            HLSUrl: recording.HLSUrl,
+                            JtrStorageDevice: "BigScreenJtr",
+                            LastViewedPosition: recording.LastViewedPosition,
+                            path: recording.path,
+                            RecordingId: recording.RecordingId,
+                            StartDateTime: recording.StartDateTime,
+                            Title: recording.Title,
+                            TranscodeComplete: recording.TranscodeComplete === 1 ? true : false
+                        });
 
-              recordingForDB.save(function(err) {
-                if (err) throw err;
-                console.log("recording " + recordingForDB.Title + " saved in db");
-              });
-
+                        recordingForDB.save(function (err) {
+                            if (err) throw err;
+                            console.log("recording " + recordingForDB.Title + " saved in db");
+                        });
+                    }
+                }
             }
-          }
+
+            console.log("done for now");
+
         }
-
-        console.log("done for now");
-
-      }
     });
 })
 
